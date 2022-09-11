bc824891075aa1488931f8e7e82639746d5216a1
samukhin <47834890+samukhin@users.noreply.github.com>
2022-09-11 19:44:46 +0300

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,13 @@
+apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances <your-package-here> | grep "^\w" | sort -u
+
+
+dpkg-query -W --showformat='${Package}\t${Priority}\n'
+
+dpkg-query -W --showformat='${Package}\t${Depends}\n'
+
+#Отметить все пакеты как установленные автоматически
+aptitude markauto $(dpkg-query -W --showformat='${Package} ')
+
+dpkg -l | grep '^rc' | awk '{print $2}' | xargs dpkg --purge
+
+apt-cache depends --installed --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $(apt list --installed 2>/dev/null | awk -F '/' 'NR>1{print $1}') > list
