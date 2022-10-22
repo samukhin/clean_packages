968f6baeb2a9e0fbba43bf5cad454f19eff54f62
samukhin <47834890+samukhin@users.noreply.github.com>
2022-10-22 21:50:26 +0300

    Create post_install_debian.sh

---
+++ post_install_debian.sh
@@ -0,0 +1,18 @@
+*#Debian 9-11
+
+apt purge --autoremove (локальные пакеты)
+apt purge --autoremove linux-image-(старые)
+
+apt purge --autoremove gcc-(старый) installation-report
+
+apt purge --autoremove xterm*
+apt purge --autoremove libreoffice*
+apt purge --autoremove avahi*
+apt purge --autoremove packagekit*
+apt purge --autoremove gnome-keyring*
+apt purge --autoremove apparmor*
+apt purge --autoremove rsyslog*
+apt purge --autoremove cron anacron
+
+#Debian 11
+apt purge --autoremove pipewire*
